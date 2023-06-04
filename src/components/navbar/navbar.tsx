3e57aaba942c51080ea802847d0da444bebfda2a
Benjamin Velliquette <BenjaminVelliquette@gmail.com>
2023-06-03 22:08:02 -0400

    extract nav button to own component

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch, SetStateAction } from 'react'
 import { Route, appRoutes } from '../../views/router/routes'
 import styles from './navbar.module.scss'
 import classNames from 'classnames'
+import NavbarButton from './navbar-button'
 type NavbarProps = {
 	setActiveTab: Dispatch<SetStateAction<Route>>
 	activeTab: Route
@@ -10,21 +11,14 @@ type NavbarProps = {
 const Navbar: React.FC<NavbarProps> = ({ setActiveTab, activeTab }) => {
 	return (
 		<nav className={styles['nav-menu']}>
-			{Object.values(appRoutes).map((route, index) => {
-				const className = classNames(styles['navbar-button'], {
-					[styles['highlighted']]: activeTab.label === route.label,
-				})
-
-				return (
-					<button
-						key={index}
-						className={className}
-						onClick={() => setActiveTab(route)}
-					>
-						<route.icon width={'100%'} height={'100%'} />
-					</button>
-				)
-			})}
+			{Object.values(appRoutes).map((route, index) => (
+				<NavbarButton
+					key={`button ${index}`}
+					icon={route.icon}
+					isActive={activeTab.label === route.label}
+					onClick={() => setActiveTab(route)}
+				/>
+			))}
 		</nav>
 	)
 }
