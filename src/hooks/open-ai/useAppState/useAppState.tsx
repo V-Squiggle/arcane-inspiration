1410ad3ee6496e0054fbc9fdd32739506870a666
Benjamin Velliquette <BenjaminVelliquette@gmail.com>
2023-06-13 15:08:53 -0400

    Break out components into separate files (fast refresh support)

--- src/hooks/open-ai/useAppState/useAppState.tsx
+++ src/hooks/open-ai/useAppState/useAppState.tsx
@@ -1,36 +1,8 @@
-import { createContext, useContext, useState } from 'react'
-
-type ContextType = {
-	openAiToken: string
-	setOpenAiToken: React.Dispatch<React.SetStateAction<string>> | undefined
-}
-
-type ContainerProps = {
-	children: React.ReactNode
-}
-
-const AppStateContext = createContext<ContextType>({
-	openAiToken: '',
-	setOpenAiToken: undefined,
-})
-
-const AppStateProvider = (props: ContainerProps) => {
-	const [openAiToken, setOpenAiToken] = useState('')
-
-	return (
-		<AppStateContext.Provider value={{ openAiToken, setOpenAiToken }}>
-			{props.children}
-		</AppStateContext.Provider>
-	)
-}
-
-export { AppStateProvider }
+import { useContext } from 'react'
+import { AppStateContext } from './appStateProvider'
 
 export const useAppState = () => {
 	const { openAiToken, setOpenAiToken } = useContext(AppStateContext)
-	if (setOpenAiToken === undefined) {
-		throw new Error('setOpenAiToken must be used within a AppStateProvider')
-	}
 
 	return { openAiToken, setOpenAiToken }
 }
